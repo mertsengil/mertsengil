747c2d05f1e522b917976ef3b5443a9346c9d94d
Mert Şengil <89454774+mertsengil@users.noreply.github.com>
2021-12-24 14:02:45 +0300

    Add files via upload

---
+++ registered.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Hakkımızda</title>
+<link rel="stylesheet" href="anasayfa.css">    
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link href="https://fonts.googleapis.com/css2?family=Creepster&family=Gloria+Hallelujah&family=Oxygen:wght@300&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <div id="menu">
+        <font><i class="fas fa-dumbbell"></i> MERTFIT <span>| Hayat Sporla Güzel...</span></font>
+        
+        <ul class="menu">
+            <li><a href="anasayfa.html">ANASAYFA</a></li>
+            <li><a href="galeri.html">GALERİ</a></li>
+            <li><a href="hakkimizda.html">HAKKIMIZDA</a></li>
+            <li><a href="kayit.php">ÜYE OL</a></li>
+        </ul>
+
+        <i class="fas fa-search"></i>
+        <i class="fab fa-instagram"></i>
+        <i class="fab fa-twitter"></i>
+        <i class="fas fa-phone-volume"></i>
+    </div>
+        <div id="container">
+        
+        <section>
+            <h1 id="registered">BAŞARIYLA KAYIT OLDUNUZ</h1>
+
+            <span id="kurallar">Spor Salonu Kuralları:</span>
+                
+            <ul>
+                <li>Kulüp girişinde galoş giyilmesi, varsa hijyenik paspasta ayakkabıların temizlenmesi zorunludur.</li>
+                <li>ÜYE’nin GYMSTOP’ta yer alan ekipmanları kullanımı tamamen kendi risk ve sorumluluğu altındadır.</li>
+                <li>GYMSTOP’a ateşli, patlayıcı, parlayıcı silahlar ve kesici veya delici aletlerle girilmesi kesinlikle yasaktır.</li>
+                <li>ÜYE’nin GYMSTOP içerisinde sigara içmesi kesinlikle yasaktır.</li>
+                <li>ÜYE’nin kendi hizmetinde çalışan özel korumaları, yardımcıları ve şoförleri GYMSTOP ‘a giremez ve tesisi kullanamaz.</li>
+                <li>Klima ve müzik sistemi gibi cihazların görevlilerin haberi olmadan kullanılması yasaktır.</li>
+                <li>Kulüpte çalan müzik, üyelerin kişisel isteklerine göre düzenlenememektedir.</li>
+                <li>GYMSTOP ‘a hayvan ile girilmesi yasaktır.</li>
+                <li>Kulübe dışarıdan yiyecek getirmek, kulüpte satılanlar dışında, kulüpte sıcak ya da soğuk, kokulu ya da kokusuz yiyecek yemek yasaktır.</li>
+                <li>Elektrikle çalışan hiçbir alet, ÜYE tarafından açılamaz, kapatılamaz, ayarlanamaz.</li>
+                <li>Kulübün çeşitli yerlerinde bulunan televizyonların kanallarını, ses düzeylerini ve kameraların görüş açılarını değiştirmek veya bu cihazları açmak, kapatmak, ayarlamak yasaktır.</li>
+                <li>İçecek şişeleri, ambalajlar, plastik bardak, galoş vb. malzemeler, kullanıldıktan sonra, çöp kutularına atılmalıdır.</li>
+                <li>ÜYE, kulüp giriş çıkışlarında bulunan turnikeleri, usulüne uygun olarak kullanmak zorundadır.</li>
+                <li>ÜYE, tesisi/kulübü kullanırken ve/veya tesiste/kulüpte kaldığı süre içinde oluşabilecek herhangi bir sakatlanma ya da sağlık probleminden GYMSTOP ‘ın, GYMSTOP’ın çalışanlarının veya GYMSTOP yönetici veya ortaklarının hiçbir suretle hukuki ve/veya cezai olarak sorumlu olmadığını, GYMSTOP ve/veya kulüp yönetici ve çalışanlarını bu nedenlerle dava etmeyeceğini ve hak talebinde bulunmayacağını peşinen kabul, beyan ve taahhüt eder.</li>
+                <li>ÜYE, spor aletlerinin kullanımı esnasında sıra bekleyebileceğini, GYMSTOP içerisinde, soyunma odalarında, duşlarda, grup derslerinde, kulüp giriş ve çıkışlarında yoğunluk olabileceğini, GYMSTOP’ın bu hususa ilişkin herhangi bir yükümlülüğü bulunmadığını, bunun hiçbir şekilde ayıplı hizmet olarak addedilemeyeceğini şimdiden kabul, beyan ve taahhüt eder.</li>
+                <li> ÜYE, GYMSTOP ‘ın bütün ekipmanlarını kullanırken işbu Faydalanma Kuralları’nda belirtilen kullanım prosedürlerine uymak zorundadır. GYMSTOP ‘ta bütün birimlerde üyelerin hijyeni, sağlığı ve güvenliği düşünülerek hazırlanan Faydalanma Kuralları’nda belirtilen prosedür ve kullanım kurallarına uyulması zorunludur.</li>
+                2
+
+
+            </ul>
+
+
+
+        </section>
+    </div>
+    <div id="footer">
+        <font id="footerlogo"><i class="fas fa-dumbbell"></i> MERTFIT</font>
+        <h4 id="tumhaklar">Tüm Hakları Saklıdır &copy; | 2021</h4> 
+        <h3 id="adres">Adres: Küçükyalı, İnönü Cd. No:4, 34854, 34854 Maltepe/İstanbul</h3>
+    </div>
+   <script src="scriptweb.js"></script>
+</body>
+</html>
