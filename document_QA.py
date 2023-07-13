859c4fff06dc2522f885dff26c0d85aecbb0948f
Mert Şengil <89454774+mertsengil@users.noreply.github.com>
2023-07-13 23:18:13 +0300

    Add files via upload

---
+++ document_QA.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import numpy as np
+from langchain.document_loaders import CSVLoader
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.chains import RetrievalQA
+from langchain.llms import OpenAI
+import os
+import streamlit as st
+from PIL import Image
+
+
+
+os.environ["OPENAI_API_KEY"] = "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"
+def document_QA(df,query):
+
+    loader = CSVLoader(file_path=df)
+    index_creator = VectorstoreIndexCreator()
+    docsearch = index_creator.from_loaders([loader])
+    chain = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff",
+                                        retriever=docsearch.vectorstore.as_retriever(), input_key="question")
+
+    query = query
+    response = chain({"question": query})
+    return response['result']
+
+
+
+
+def main():
+    img = Image.open('/Users/mertsengil/Desktop/ARKAS/icono_empresa_ARKAS.png',)
+
+
+
+
+    st.markdown("""<div class="nav">
+<h1 style='text-align: center; display: inline-block; color: #fec301;'>QA BASED CHATBOT FOR DOCUMENTS</h1>
+</div>""",unsafe_allow_html=True)
+
+#     st.markdown("""<div class="nav" style="display: flex; gap: 10px;">
+# <img src="https://romeu.com/wp-content/uploads/2020/10/icono_empresa_ARKAS.png" alt="" height="150px" />
+# <h1 style='text-align: left; display: inline-block; color: #fec301;'>QA BASED CHATBOT FOR DOCUMENTS</h1>
+# </div>""", unsafe_allow_html=True)
+
+    st.subheader("""Select your document.
+                    available documents: 1- '.../ARKAS/ARL PL Result.csv'
+                         2- '.../ARKAS/Service Based Avr SF.csv' """)
+
+    # Soru barını beyaz yapın
+    st.markdown(
+        """
+        <style>
+            .stTextInput>div>div>input {
+                background-color: #FFFFFF;
+                color: #000000;
+            }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+
+    user_input = st.text_input("document: ('1' or '2')")
+
+    if user_input:
+
+        if user_input == '1':
+            st.write('document 1 is chosen')
+            query = st.text_input("Ask question: ")
+            if query:
+                df="/Users/mertsengil/Desktop/ARKAS/Profit and Loss.csv"
+                response = document_QA(df,query)
+                st.write(response)
+
+        elif user_input == '2':
+            st.write('document 2 is chosen')
+            query = st.text_input("Ask question: ")
+            if query:
+                df="/Users/mertsengil/Desktop/ARKAS/Service Based Avr SF.csv"
+                response = document_QA(df,query)
+                st.write(response)
+        else:
+            st.write("document not found.")
+
+if __name__ == "__main__":
+    main()
